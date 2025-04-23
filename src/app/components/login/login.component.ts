4b60c324a57137bc311b36921028137883108d7a
SamiChafik <sami.chafik00@gmail.com>
2025-04-23 15:36:07 +0100

    added html and style of the login page

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  credentials = {
+    email: '',
+    password: ''
+  };
 
+  onSubmit() {
+  }
 }
