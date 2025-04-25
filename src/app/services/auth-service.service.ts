d94e3547f62187b3b108de0d4f280bb53273a82a
SamiChafik <sami.chafik00@gmail.com>
2025-04-25 17:17:47 +0100

    added method to save user info using api

--- src/app/services/auth-service.service.ts
+++ src/app/services/auth-service.service.ts
@@ -1,9 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthServiceService {
+export class AuthService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { 
+
+  }
+  apiurl='http://localhost:3000/user';
+
+  RegisterUser(inputdata:any){
+    return this.http.post(this.apiurl,inputdata)
+  }
+
 }
