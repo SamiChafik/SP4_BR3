ea463e4c58a554878f1222bbe7ae4823c5e82366
SamiChafik <sami.chafik00@gmail.com>
2025-04-25 17:18:24 +0100

    added new route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { PostListComponent } from './components/post-list/post-list.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'articles', component: PostListComponent },
     { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
     { path: '**', redirectTo: '' }
 ];
