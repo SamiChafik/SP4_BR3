945ba61f2ed4c0e74e588361c7fec38fceecea80
SamiChafik <sami.chafik00@gmail.com>
2025-04-27 17:10:00 +0100

    showing links based on if user is logged in or not + logout function

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
 
-  logout(){};
+  constructor(public authService: AuthService) {}
 
+  Logout(): void {
+    this.authService.logout();
+  }
 }
