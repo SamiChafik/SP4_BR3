2ec7fc15a7d13fdba5c00411ae9c5ac05659fb44
SamiChafik <sami.chafik00@gmail.com>
2025-04-27 17:57:31 +0100

    checking if the user is logged in to allow routing to app componants

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -1,5 +1,26 @@
-import { CanActivateFn } from '@angular/router';
+import { inject, Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+  private service = inject(AuthService);
+  private router = inject(Router);
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+   
+    if (this.service.isLoggedIn()) {
+      return true;
+    }
+    else {
+      this.router.navigate(['login']);
+      return false;
+    }
+  }
+
+}
