29c5de20c183f073c9f033727fdb0e79cab5506e
SamiChafik <sami.chafik00@gmail.com>
2025-04-27 17:08:21 +0100

    verifying user email and password if they exist in the user array in db.json then redirecting user to home

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,8 @@
-import { Component, inject } from '@angular/core';
-import { FormBuilder, ReactiveFormsModule, Validator, Validators } from '@angular/forms';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,14 +12,34 @@ import { CommonModule } from '@angular/common';
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-  fb = inject(FormBuilder);
+  private builder = inject(FormBuilder);
+  private service = inject(AuthService);
+  private router = inject(Router);
 
-  form = this.fb.nonNullable.group({
-    email: ['', Validators.required],
-    password: ['', Validators.required]
-  })
+  result: any;
 
-  onSubmit(): void {
-    
+  loginform = this.builder.group({
+    password: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]]
+  });
+
+  proceedlogin() {
+    if (this.loginform.valid) {
+      const { email, password } = this.loginform.value;
+      this.service.LoginUser(email!, password!).subscribe({
+        next: (user) => {
+          sessionStorage.setItem('userId', user.id);
+          sessionStorage.setItem('username', user.username);
+          this.router.navigate(['']);
+        },
+        error: (err) => {
+          console.error('Login failed:', err);
+          this.loginform.setErrors({ invalidCredentials: true });
+        }
+      });
+    } else {
+      this.loginform.markAllAsTouched();
+    }
   }
+  
 }
